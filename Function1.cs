6716f2895463b0302935fc16d86043e8d48deb77
Selvarajks <selvarajks@gmail.com>
2019-02-24 18:35:48 +0800

    My first Commit

---
+++ Function1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Formatting;
+
+namespace SelvaFuncTimerTrigger
+{
+    public static class SelvaTimerFunction
+    {        
+
+        [FunctionName("SelvaTimerFunction")]
+        public static async Task Run(TimerInfo myTimer, ILogger log)
+        {
+
+            log.LogInformation($"C# Selva Timer trigger function executed at: {DateTime.Now}");
+
+            var client = new HttpClient();
+            var resp = await client.GetAsync("https://selvafunctionapps.azurewebsites.net/api/RestApiCall?name=selva");
+
+            var content = await resp.Content.ReadAsStringAsync();
+            log.LogInformation($"Response returned from the API => " + content);
+
+        }
+
+    }
+}
